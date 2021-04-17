83c527d3c274fd272113133b6dc24c98784576ae
Phi <phipgn@gmail.com>
2021-04-17 15:38:18 +0700

    update

---
+++ src/main/java/phipgn/sloppy_test2/App.java
@@ -0,0 +1,13 @@
+package phipgn.sloppy_test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
