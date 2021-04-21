8c71995a277da0c5f9ff5e6557840791825aef80
Phi <phipgn@gmail.com>
2021-04-21 18:47:55 +0700

    update

--- src/test/java/phipgn/sloppy_test/tests/SearchTest.java
+++ src/test/java/phipgn/sloppy_test/tests/SearchTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -15,11 +14,6 @@ import phipgn.sloppy_test.pages.HomePage;
 public class SearchTest extends BaseTest
 {
 	private HomePage homePage;
-	
-	private SearchTest() { }
-	
-	@BeforeTest
-	public void beforeTest() { }
 	
 	@BeforeMethod
 	public void beforeMethod() {
